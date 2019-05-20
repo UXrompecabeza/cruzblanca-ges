72838ed588f61523cbcd7d0ef5c1ba4f185b5dbf
CarlaZumaran <carla.z@rompecabeza.cl>
2019-05-20 15:56:34 -0400

    Cambios en GES

--- js/funciones-ges.js
+++ js/funciones-ges.js
@@ -1,17 +1,37 @@
+$('.input-date input.datepicker').on('change', function() {
+  $('.pickCenter').removeClass('disabled');
+  $('.pickCenter').click(function() {
+    window.location.href='ges-consulta.html';
+  })
+})
+
+$('.btn-center input').click( function() {
+
+  $('.containerCenter').removeClass('selectedCenter');
+  $(this).parent().addClass('selectedCenter');
+  $('html, body').animate({
+    scrollTop: $('.btnGes').offset().top
+  }, 700);
+})
+
 $('.btn-patologia').click( function() {
   $('.btn-patologia').removeClass('activeGes');
   $(this).addClass('activeGes');
-  $('.next').removeClass('disabled');
+  $('.btnGes').removeClass('disabled');
   $('html, body').animate({
-    scrollTop: $('.next').offset().top
-}, 700);
+    scrollTop: $('.btnGes').offset().top
+  }, 700);
+
   $('.next').click(function() {
-    window.location.href='ges-consulta.html';
+    window.location.href='ges-centro.html';
   })
 })
 
 $('.accordionGes .containerRadius input').click(function() {
   $('.buy').removeClass('disabled');
+  $('html, body').animate({
+    scrollTop: $('.btnGes').offset().top
+  }, 700);
   $('.buy').click(function() {
     window.location.href='ges-comprar.html';
   })
@@ -39,4 +59,17 @@ $(function () {
           items: 1
       })
   }
+});
+
+$('.item').click(function() {
+  $('.container-btnes').toggleClass('hide');
+  $('.alertaBtnes').toggleClass('hide');
+})
+
+$(".datepicker").datepicker({
+  dateFormat: "dd/mm/yy",
+  monthNames: [ "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" ],
+  firstDay: 1,
+  dayNamesMin: [ "Dom", "Lun", "Mar", "Mie", "Jue", "Vie", "Sab" ],
+  minDate: new Date(2019, 05, 20)
 });
